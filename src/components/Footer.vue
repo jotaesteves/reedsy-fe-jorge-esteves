9f9dd638629a7816595f3ec96c86abd71b7db523
Jorge Esteves <jorge@Steves-MBP.Home>
2020-04-07 14:08:09 +0100

    git init

---
+++ src/components/Footer.vue
@@ -0,0 +1,32 @@
+<template>
+      <footer>
+      <div class="container">
+        <nav>
+          <ul>
+            <li>
+              <a>Blog</a>
+            </li>
+            <li>
+              <a>About</a>
+            </li>
+            <li>
+              <a>Privacy</a>
+            </li>
+            <li>
+              <a>Terms</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
